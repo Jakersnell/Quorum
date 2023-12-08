0d3deb149f5f218cfb341400db987b7f6bf243a7
Parrisu <parris.a.jones@gmail.com>
2023-12-08 12:35:44 -0800

    Full-stack smoke test

--- Quorum/src/main/java/com/skilldistillery/quorum/data/UserDAO.java
+++ Quorum/src/main/java/com/skilldistillery/quorum/data/UserDAO.java
@@ -1,5 +1,9 @@
 package com.skilldistillery.quorum.data;
 
+import com.skilldistillery.quorum.entities.User;
+
 public interface UserDAO {
+	
+	User authenticateUser (String username, String password);
 
 }
--- Quorum/src/main/java/com/skilldistillery/quorum/data/UserDaoImpl.java
+++ Quorum/src/main/java/com/skilldistillery/quorum/data/UserDaoImpl.java
@@ -2,6 +2,8 @@ package com.skilldistillery.quorum.data;
 
 import org.springframework.stereotype.Service;
 
+import com.skilldistillery.quorum.entities.User;
+
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
@@ -12,5 +14,23 @@ public class UserDaoImpl implements UserDAO {
 
 	@PersistenceContext
 	private EntityManager em;
+
+	@Override
+	public User authenticateUser(String username, String password) {
+		String jpql = "SELECT u FROM User u WHERE username = :username AND password = :password AND u.enabled = true";
+		User user = null;
+		
+		try {
+			user = em.createQuery(jpql, User.class)
+					.setParameter("username", username)
+					.setParameter("password", password)
+					.getSingleResult();
+		} catch (Exception e) {
+			System.err.print("Invalid user: " + username);
+			e.printStackTrace();
+		}
+
+		return user;
+	}
 
 }
