9913fb426f6bdefd9298e325609fb0d39f293a2a
Jacob Snell <jake.r.snell@gmail.com>
2023-12-10 10:47:47 -0700

    Add Null check for DayOfWeekConverter

--- JPAQuorum/src/main/java/com/skilldistillery/quorum/entities/DayOfWeekConverter.java
+++ JPAQuorum/src/main/java/com/skilldistillery/quorum/entities/DayOfWeekConverter.java
@@ -12,16 +12,24 @@ public class DayOfWeekConverter implements AttributeConverter<HashSet<DayOfWeek>
 
 	@Override
 	public String convertToDatabaseColumn(HashSet<DayOfWeek> attribute) {
-		return attribute.stream().map(Enum::name).collect(Collectors.joining("/"));
+		if (attribute != null) {
+			return attribute.stream().map(Enum::name).collect(Collectors.joining("/"));
+		} else {
+			return null;
+		}
 	}
 
 	@Override
 	public HashSet<DayOfWeek> convertToEntityAttribute(String dbData) {
-		HashSet<DayOfWeek> daysOfWeek = new HashSet<>();
-		for (String day : dbData.split("/")) {
-			daysOfWeek.add(DayOfWeek.valueOf(day));
+		if (dbData != null) {
+			HashSet<DayOfWeek> daysOfWeek = new HashSet<>();
+			for (String day : dbData.split("/")) {
+				daysOfWeek.add(DayOfWeek.valueOf(day));
+			}
+			return daysOfWeek;
+		} else {
+			return null;
 		}
-		return daysOfWeek;
 	}
 
 }
