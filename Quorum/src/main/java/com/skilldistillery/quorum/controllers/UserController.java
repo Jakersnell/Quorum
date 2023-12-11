197f3d2de71858c73c22716cb9bcc6ccafc588f5
Jacob Snell <jake.r.snell@gmail.com>
2023-12-11 09:22:42 -0700

    add login GET/POST to UserController

--- Quorum/src/main/java/com/skilldistillery/quorum/controllers/UserController.java
+++ Quorum/src/main/java/com/skilldistillery/quorum/controllers/UserController.java
@@ -3,20 +3,52 @@ package com.skilldistillery.quorum.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.skilldistillery.quorum.data.UserDAO;
+import com.skilldistillery.quorum.entities.User;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class UserController {
 
 	@Autowired
 	private UserDAO userDao;
-	
-	@RequestMapping(path = {"/", "home.do"})
+
+	@GetMapping({ "/", "home.do" })
 	public String home(Model model) {
 		model.addAttribute("SMOKETEST", userDao.authenticateUser("jtest", "jtest"));
 		return "home";
+	}
+
+	@GetMapping({ "/login", "login.do" })
+	public String loginGet(HttpSession session) {
+		if (session.getAttribute("loggedUser") != null) {
+			return "redirect:/home.do";
+		}
+		return "login";
+	}
+
+	@PostMapping({ "/login", "login.do" })
+	public String loginPost(@ModelAttribute User user, HttpSession session) {
+		String redirect = "redirect:/home.do";
+
+		if (session.getAttribute("loggedUser") == null) {
+
+			User searchedUser = userDao.authenticateUser(user.getUsername(), user.getPassword());
+
+			if (searchedUser != null) {
+				session.setAttribute("loggedUser", searchedUser);
+			} else {
+				redirect = "redirect:/login.do"; //change to sign-up
+			}
+			
+		}
+
+		return redirect;
 	}
 
 }
