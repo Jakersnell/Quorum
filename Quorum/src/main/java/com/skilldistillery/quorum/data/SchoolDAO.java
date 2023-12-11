71f9c521e502c56055ed72565b28325de2d02d3d
Jacob Snell <jake.r.snell@gmail.com>
2023-12-11 16:16:42 -0700

    Add SchoolDAOImple

--- Quorum/src/main/java/com/skilldistillery/quorum/data/SchoolDAO.java
+++ Quorum/src/main/java/com/skilldistillery/quorum/data/SchoolDAO.java
@@ -1,8 +1,11 @@
 package com.skilldistillery.quorum.data;
 
+import java.util.List;
+
 import com.skilldistillery.quorum.entities.School;
 
 public interface SchoolDAO {
-	School getAll();
+	List<School> getAll();
+
 	School getById(int id);
 }
