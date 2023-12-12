905e571084e0df6d1355b874fa8cf2549e126562
Jacob Snell <jake.r.snell@gmail.com>
2023-12-12 11:15:20 -0700

    Add getUserFollowers and getUserFollowing to UserDAO

--- Quorum/src/main/java/com/skilldistillery/quorum/data/UserDAO.java
+++ Quorum/src/main/java/com/skilldistillery/quorum/data/UserDAO.java
@@ -1,5 +1,7 @@
 package com.skilldistillery.quorum.data;
 
+import java.util.List;
+
 import com.skilldistillery.quorum.entities.User;
 
 public interface UserDAO {
@@ -11,5 +13,9 @@ public interface UserDAO {
 	public User getUserById(int id);
 
 	User getUserByUsername(String username);
+
+	List<User> getUserFollowers(User user);
+	
+	List<User> getUserFollowing(User user);
 
 }
